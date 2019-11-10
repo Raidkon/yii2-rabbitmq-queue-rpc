b68e5b31b4cc2e73d9a98c7d4fcf8fa54a8397ff
Никита Медведев <n@raidkon.com>
2019-11-10 18:23:08 +0500

    Fix redelivery

--- src/Queue.php
+++ src/Queue.php
@@ -8,29 +8,26 @@
 
 namespace raidkon\yii2\ServerRpc;
 
-use InvalidArgumentException;
-use Yii;
-use yii\base\InvalidConfigException;
-use yii\helpers\VarDumper;
-use yii\log\Logger;
-use yii\queue\amqp_interop\Queue as ExtendsQueue;
+use Error;
+use Exception;
 use Interop\Amqp\AmqpConsumer;
 use Interop\Amqp\AmqpMessage;
 use Interop\Amqp\AmqpQueue;
 use Interop\Amqp\AmqpTopic;
 use Interop\Amqp\Impl\AmqpBind;
-use yii\queue\ExecEvent;
-use yii\queue\JobInterface;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\queue\amqp_interop\Queue as ExtendsQueue;
 
 class Queue extends ExtendsQueue
 {
-    public $exchangeType  = AmqpTopic::TYPE_TOPIC;
-    public $queueName     = 'server.queue.topic';
-    public $exchangeName  = 'global.topic';
-    public $user          = 'guest';
-    public $password      = 'guest';
-    public $port          = 5672;
-    public $driver        = ExtendsQueue::ENQUEUE_AMQP_LIB;
+    public $exchangeType = AmqpTopic::TYPE_TOPIC;
+    public $queueName = 'server.queue.topic';
+    public $exchangeName = 'global.topic';
+    public $user = 'guest';
+    public $password = 'guest';
+    public $port = 5672;
+    public $driver = ExtendsQueue::ENQUEUE_AMQP_LIB;
     
     public $serverRpcComponentName = 'serverRpc';
     
@@ -47,29 +44,29 @@ class Queue extends ExtendsQueue
         $this->context->subscribe($consumer, function (AmqpMessage $message, AmqpConsumer $consumer) {
             $comp = Yii::$app->get($this->serverRpcComponentName);
     
-            if (!($comp instanceof Server)){
+            if (!($comp instanceof Server)) {
                 throw new InvalidConfigException('Component ' . $this->queue . ' must be raidkon\yii2\ServerRpc\Queue');
             }
     
             try {
                 if ($message->isRedelivered()) {
                     $consumer->acknowledge($message);
-            
+    
                     $this->redeliver($message);
-            
+    
                     return true;
                 }
         
-                if (!$comp->handleMessage($message,$consumer)) {
+                if (!$comp->handleMessage($message, $consumer)) {
                     $this->redeliver($message);
                 }
                 $consumer->acknowledge($message);
-            } catch (\Exception $error) {
+            } catch (Exception $error) {
                 $consumer->acknowledge($message);
                 var_dump($error->getMessage());
                 var_dump($error->getTraceAsString());
                 return true;
-            } catch (\Error $error) {
+            } catch (Error $error) {
                 $consumer->acknowledge($message);
                 var_dump($error->getMessage());
                 var_dump($error->getTraceAsString());
@@ -82,6 +79,28 @@ class Queue extends ExtendsQueue
         
         
         $this->context->consume();
+    }
+    
+    protected function redeliver(AmqpMessage $message)
+    {
+        $attempt = $message->getProperty(self::ATTEMPT, 1);
+        
+        $newMessage = $this->context->createMessage($message->getBody(), $message->getProperties(), $message->getHeaders());
+        $newMessage->setDeliveryMode($message->getDeliveryMode());
+        $newMessage->setProperty(self::ATTEMPT, ++$attempt);
+        
+        
+        if ($user_id = $newMessage->getHeader('user_id')) {
+            if ($user_id && $user_id != $this->user) {
+                $newMessage->setHeader('user_id', $this->user);
+                $newMessage->setProperty('real_user_id', $user_id);
+            }
+        }
+        
+        $this->context->createProducer()->send(
+            $this->context->createQueue($this->queueName),
+            $newMessage
+        );
     }
     
     protected function setupBroker()
@@ -99,7 +118,7 @@ class Queue extends ExtendsQueue
         $exchange->setType($this->exchangeType);
         $exchange->addFlag(AmqpTopic::FLAG_DURABLE);
         $this->context->declareTopic($exchange);
-        $this->context->bind(new AmqpBind($queue, $exchange,'#'));
+        $this->context->bind(new AmqpBind($queue, $exchange, '#'));
         
         $this->setupBrokerDone = true;
     }
--- src/Server.php
+++ src/Server.php
@@ -62,7 +62,7 @@ class Server extends BaseObject implements BootstrapInterface
     
         $comp = Yii::$app->get($this->queueName);
     
-        if ($comp instanceof Queue){
+        if ($comp instanceof Queue) {
             $this->_queue = $comp;
         } else {
             throw new InvalidConfigException('Component ' . $this->queueName . ' must be raidkon\yii2\ServerRpc\Queue');
@@ -88,7 +88,7 @@ class Server extends BaseObject implements BootstrapInterface
      * @throws InvalidDestinationException
      * @throws InvalidMessageException
      */
-    public function pushMessage($message,string $route,array $params = [])
+    public function pushMessage($message, string $route, array $params = [])
     {
         $messageId = uniqid('', true);
         
@@ -106,25 +106,25 @@ class Server extends BaseObject implements BootstrapInterface
         return $this->pushMessageNow($messageId, $message, $route, $params);
     }
     
-    public function pushMessageNow($messageId, $message,string $route,array $params = [])
+    public function pushMessageNow($messageId, $message, string $route, array $params = [])
     {
-        if (!is_string($message)){
+        if (!is_string($message)) {
             $message = json_encode($message);
         }
     
-        $route = preg_replace_callback('/{(?<var>[a-z0-9_]+?)}/is',function($matches) use ($params) {
-            if (!key_exists($matches['var'],$params)){
+        $route = preg_replace_callback('/{(?<var>[a-z0-9_]+?)}/is', function ($matches) use ($params) {
+            if (!key_exists($matches['var'], $params)) {
                 NotBound::throw(['var' => $matches['var']]);
             }
             return $params[$matches['var']];
         
-        },$route);
-    
-    
+        }, $route);
+        
+        
         $context = $this->_queue->getContext();
     
         $message = $context->createMessage($message);
-        $message->setDeliveryMode(AmqpMessage::DELIVERY_MODE_PERSISTENT);
+        $message->setDeliveryMode(AmqpMessage::DELIVERY_MODE_NON_PERSISTENT);
         $message->setMessageId($messageId);
         $message->setTimestamp(time());
         $message->setRoutingKey($route);
@@ -132,7 +132,7 @@ class Server extends BaseObject implements BootstrapInterface
         $exchange = $context->createTopic($this->_queue->exchangeName);
     
         $producer = $context->createProducer();
-        $producer->send($exchange,$message);
+        $producer->send($exchange, $message);
         return $messageId;
     }
     
@@ -146,21 +146,21 @@ class Server extends BaseObject implements BootstrapInterface
      * @throws InvalidDestinationException
      * @throws InvalidMessageException
      */
-    public function sendMessage($message,$answerTo,$answerCorrelationId,$fromUserId = null)
+    public function sendMessage($message, $answerTo, $answerCorrelationId, $fromUserId = null)
     {
-        if (!$fromUserId){
-            $fromUserId = ArrayHelper::getValue(Yii::$app,'user.id');
+        if (!$fromUserId) {
+            $fromUserId = ArrayHelper::getValue(Yii::$app, 'user.id');
         }
         
         $messageId = uniqid('', true);
         
         $context = $this->_queue->getContext();
-        
-        if (!is_string($message) && !is_array($message)){
+    
+        if (!is_string($message) && !is_array($message)) {
             MessageIncorrectTypeException::throw();
         }
-        
-        if (is_array($message)){
+    
+        if (is_array($message)) {
             $message = json_encode($message);
         }
         
@@ -168,18 +168,18 @@ class Server extends BaseObject implements BootstrapInterface
         $message->setDeliveryMode(AmqpMessage::DELIVERY_MODE_PERSISTENT);
         $message->setMessageId($messageId);
         $message->setTimestamp(time());
-        $message->setProperty('correlation_id',$answerCorrelationId);
-        $message->setProperty('user_id',$this->_queue->user);
+        $message->setProperty('correlation_id', $answerCorrelationId);
+        $message->setProperty('user_id', $this->_queue->user);
         $message->setRoutingKey($answerTo);
-        
-        if ($fromUserId){
-            $message->setProperty('real_user_id',$fromUserId);
+    
+        if ($fromUserId) {
+            $message->setProperty('real_user_id', $fromUserId);
         }
         
         $exchange = $context->createTopic('');
     
         $producer = $context->createProducer();
-        $producer->send($exchange,$message);
+        $producer->send($exchange, $message);
         return $messageId;
     }
     
@@ -214,15 +214,15 @@ class Server extends BaseObject implements BootstrapInterface
     {
         if ($app instanceof WebApp) {
             $app->controllerMap[$this->getCommandId()] = [
-                    'class'  => $this->controllerClass,
+                    'class' => $this->controllerClass,
                     'server' => $this,
                 ] + $this->commandOptions;
             
         } elseif ($app instanceof ConsoleApp) {
             $app->controllerMap[$this->getCommandId()] = [
-                'class'  => $this->commandClass,
-                'server' => $this,
-            ] + $this->commandOptions;
+                    'class' => $this->commandClass,
+                    'server' => $this,
+                ] + $this->commandOptions;
         }
     }
     
@@ -233,13 +233,13 @@ class Server extends BaseObject implements BootstrapInterface
      */
     public function findUser(string $user_id): IUser
     {
-        if (!empty($this->userMap[$user_id])){
+        if (!empty($this->userMap[$user_id])) {
             $user_id = $this->userMap[$user_id];
         } elseif (empty($user_id)) {
             return null;
         }
-        
-        if (!is_subclass_of($this->userClass,  IUser::class)) {
+    
+        if (!is_subclass_of($this->userClass, IUser::class)) {
             throw new InvalidConfigException('Class user must be ' . IUser::class);
         }
         
@@ -259,7 +259,7 @@ class Server extends BaseObject implements BootstrapInterface
      */
     public function createCmd($user, string $route, $params = null): ICommand
     {
-        return new Command($this,$user,$route,$params);
+        return new Command($this, $user, $route, $params);
     }
     
     /**
@@ -274,22 +274,22 @@ class Server extends BaseObject implements BootstrapInterface
         $user_id = $safe_user_id = $message->getHeader('user_id');
         $real_user_id = $message->getProperty('real_user_id');
         $log = [
-            'type'              => 'handleMessage',
-            'from_user_id'      => $safe_user_id,
+            'type' => 'handleMessage',
+            'from_user_id' => $safe_user_id,
             'from_real_user_id' => $real_user_id,
-            'route_key'         => $message->getRoutingKey(),
-            'message'           => $message->getBody(),
-            'reply_to'          => $message->getReplyTo(),
-            'correlation_id'    => $message->getCorrelationId(),
+            'route_key' => $message->getRoutingKey(),
+            'message' => $message->getBody(),
+            'reply_to' => $message->getReplyTo(),
+            'correlation_id' => $message->getCorrelationId(),
         ];
-        $info = function($data,$sub = null){
-            Yii::info($data,static::class . '::message' . ($sub?'::' . $sub:''));
+        $info = function ($data, $sub = null) {
+            Yii::info($data, static::class . '::message' . ($sub ? '::' . $sub : ''));
         };
-        $warning = function($data,$sub = null){
-            Yii::warning($data,static::class . '::message' . ($sub?'::' . $sub:''));
+        $warning = function ($data, $sub = null) {
+            Yii::warning($data, static::class . '::message' . ($sub ? '::' . $sub : ''));
         };
         $info($log);
-        if ($real_user_id && $user_id === $this->_queue->user){
+        if ($real_user_id && $user_id === $this->_queue->user) {
             $user_id = $real_user_id;
         }
         try {
@@ -297,47 +297,47 @@ class Server extends BaseObject implements BootstrapInterface
         } catch (Throwable $throwable) {
             $user = null;
         }
-        if ($user){
+        if ($user) {
             $log['user_id'] = $user->getId();
         }
         $info($log);
-        $cmd = $this->createCmd($user,$message->getRoutingKey(),$message->getBody());
+        $cmd = $this->createCmd($user, $message->getRoutingKey(), $message->getBody());
         $cmd->initIdentity();
         $log += [
             'command' => $cmd->getCommandName(),
-            'action'  => $cmd->getActionName(),
+            'action' => $cmd->getActionName(),
         ];
-        if (!$cmd->isCall()){
+        if (!$cmd->isCall()) {
             $cmd->restoreIdentity();
-            $warning($log + ['result' => false,'reason' => 'cmd is not call'],'result');
+            $warning($log + ['result' => false, 'reason' => 'cmd is not call'], 'result');
             return true;
         }
-        
-        if (!$cmd->checkAccess(ICommand::ACCESS_CALL,ICommand::RESOURCE_MESSAGE)){
+    
+        if (!$cmd->checkAccess(ICommand::ACCESS_CALL, ICommand::RESOURCE_MESSAGE)) {
             $cmd->restoreIdentity();
-            $warning($log + ['result' => false,'reason' => 'not access'],'result');
+            $warning($log + ['result' => false, 'reason' => 'not access'], 'result');
             return true;
         }
         
         $result = $cmd->call($message);
         
         if ($result === true) {
-            $info($log + ['result' => $result,'reason' => 'result from command'],'result');
+            $info($log + ['result' => $result, 'reason' => 'result from command'], 'result');
         } else {
-            $warning($log + ['result' => $result,'reason' => 'result from command'],'result');
+            $warning($log + ['result' => $result, 'reason' => 'result from command'], 'result');
         }
         $cmd->restoreIdentity();
         return $result;
     }
     
-    protected function _findUser(?string $username):?IUser
+    protected function _findUser(?string $username): ?IUser
     {
         /** @var IUser $model */
         $model = $this->userClass;
         return $model::rpcServerFind($username);
     }
     
-    public function checkAuthUser($username,$password): bool
+    public function checkAuthUser($username, $password): bool
     {
         $cacheKey = [
             'type' => static::class,
@@ -359,29 +359,29 @@ class Server extends BaseObject implements BootstrapInterface
         
         
         $log = [
-            'type'     => 'checkAuthUser',
+            'type' => 'checkAuthUser',
             'username' => $username,
-            'password' => !empty($password)?'*' . strlen($password) . '*' . Yii::$app->security->generatePasswordHash($password) . '*':'*is_empty*',
+            'password' => !empty($password) ? '*' . strlen($password) . '*' . Yii::$app->security->generatePasswordHash($password) . '*' : '*is_empty*',
         ];
-        
-        Yii::info($log,static::class . '::access');
-        if ($this->_queue->user == $username){
+    
+        Yii::info($log, static::class . '::access');
+        if ($this->_queue->user == $username) {
             $result = $this->_queue->password == $password;
             $cache && $cache->set($cacheKey, (int)$result, 60);
-            Yii::info($log + ['result' => $result],static::class . '::access::result');
+            Yii::info($log + ['result' => $result], static::class . '::access::result');
             return $result;
         }
         
         $model = $this->_findUser($username);
-        
-        if (!$model){
-            Yii::info($log + ['result' => false],static::class . '::access::result');
+    
+        if (!$model) {
+            Yii::info($log + ['result' => false], static::class . '::access::result');
             $cache && $cache->set($cacheKey, 0, 60);
             return false;
         }
     
         $result = $model->rpcServerValidPassword($password);
-        Yii::info($log + ['result' => $result],static::class . '::access::result');
+        Yii::info($log + ['result' => $result], static::class . '::access::result');
         $cache && $cache->set($cacheKey, (int)$result, 60);
         return $result;
     }
@@ -389,106 +389,106 @@ class Server extends BaseObject implements BootstrapInterface
     public function checkResource($username, $vhost, $resource, $name, $permission)
     {
         $log = [
-            'exec_cmd'   => 'yii server-rpc/resource "' . implode('" "',func_get_args()) . '"',
-            'type'       => 'checkResource',
-            'username'   => $username,
-            'vhost'      => $vhost,
-            'resource'   => $resource,
-            'name'       => $name,
+            'exec_cmd' => 'yii server-rpc/resource "' . implode('" "', func_get_args()) . '"',
+            'type' => 'checkResource',
+            'username' => $username,
+            'vhost' => $vhost,
+            'resource' => $resource,
+            'name' => $name,
             'permission' => $permission
         ];
-        $info = function($data = [],$sub = null){
-            Yii::info($data,static::class . '::access' . ($sub?'::' . $sub:''));
+        $info = function ($data = [], $sub = null) {
+            Yii::info($data, static::class . '::access' . ($sub ? '::' . $sub : ''));
         };
         $info($log);
-        
-        if (!in_array($permission,[ICommand::ACCESS_WRITE,ICommand::ACCESS_CONFIGURE,ICommand::ACCESS_READ])){
-            $info($log + ['result' => false,'reason' => 'permission not exists'],'result');
+    
+        if (!in_array($permission, [ICommand::ACCESS_WRITE, ICommand::ACCESS_CONFIGURE, ICommand::ACCESS_READ])) {
+            $info($log + ['result' => false, 'reason' => 'permission not exists'], 'result');
             return false;
         }
-        if (!in_array($resource,[ICommand::RESOURCE_TOPIC,ICommand::RESOURCE_EXCHANGE,ICommand::RESOURCE_QUEUE])){
-            $info($log + ['result' => false,'reason' => 'resource not exists'],'result');
+        if (!in_array($resource, [ICommand::RESOURCE_TOPIC, ICommand::RESOURCE_EXCHANGE, ICommand::RESOURCE_QUEUE])) {
+            $info($log + ['result' => false, 'reason' => 'resource not exists'], 'result');
             return false;
         }
         
         // if server connect, allow everything
-        if ($this->_queue->user == $username){
-            $info($log + ['result' => true],'result');
+        if ($this->_queue->user == $username) {
+            $info($log + ['result' => true], 'result');
             return true;
         }
         
         $user = $this->_findUser($username);
-        
-        if (!$user){
-            $info($log + ['result' => false,'reason' => 'user not exists'],'result');
+    
+        if (!$user) {
+            $info($log + ['result' => false, 'reason' => 'user not exists'], 'result');
             return false;
         }
         
         $log['user_id'] = $user->getId();
     
-        $cmd = $this->createCmd($user,$name);
+        $cmd = $this->createCmd($user, $name);
         
         $log['command_name'] = $cmd->getCommandName();
         $log['command_action'] = $cmd->getActionName();
     
-        if (!$cmd->checkAccess($permission,$resource)){
-            $info($log + ['result' => false,'reason' => 'not access'],'result');
+        if (!$cmd->checkAccess($permission, $resource)) {
+            $info($log + ['result' => false, 'reason' => 'not access'], 'result');
             return false;
         }
-        
-        
-        $info($log + ['result' => true],'result');
+    
+    
+        $info($log + ['result' => true], 'result');
         return true;
     }
     
-    public function checkTopic($username, $vhost, $resource, $name, $permission,$routing_key)
+    public function checkTopic($username, $vhost, $resource, $name, $permission, $routing_key)
     {
         $log = [
-            'exec_cmd'    => 'yii server-rpc/topic "' . implode('" "',func_get_args()) . '"',
-            'type'        => 'checkTopic',
-            'username'    => $username,
-            'vhost'       => $vhost,
-            'resource'    => $resource,
-            'name'        => $name,
-            'permission'  => $permission,
+            'exec_cmd' => 'yii server-rpc/topic "' . implode('" "', func_get_args()) . '"',
+            'type' => 'checkTopic',
+            'username' => $username,
+            'vhost' => $vhost,
+            'resource' => $resource,
+            'name' => $name,
+            'permission' => $permission,
             'routing_key' => $routing_key,
         ];
-        $info = function($data = [],$sub = null) {
-            Yii::info($data,static::class . '::access' . ($sub?'::' . $sub:''));
+        $info = function ($data = [], $sub = null) {
+            Yii::info($data, static::class . '::access' . ($sub ? '::' . $sub : ''));
         };
         $info();
-        if ($this->_queue->user == $username){
-            $info($log + ['result' => true],'result');
+        if ($this->_queue->user == $username) {
+            $info($log + ['result' => true], 'result');
             return true;
         }
-        if (!in_array($permission,[ICommand::ACCESS_WRITE,ICommand::ACCESS_CONFIGURE,ICommand::ACCESS_READ])){
-            $info($log + ['result' => false,'reason' => 'permission not exists'],'result');
+        if (!in_array($permission, [ICommand::ACCESS_WRITE, ICommand::ACCESS_CONFIGURE, ICommand::ACCESS_READ])) {
+            $info($log + ['result' => false, 'reason' => 'permission not exists'], 'result');
             return false;
         }
-        if (!in_array($resource,[ICommand::RESOURCE_TOPIC])){
-            $info($log + ['result' => false,'reason' => 'resource not exists'],'result');
+        if (!in_array($resource, [ICommand::RESOURCE_TOPIC])) {
+            $info($log + ['result' => false, 'reason' => 'resource not exists'], 'result');
             return false;
         }
     
         $user = $this->_findUser($username);
     
-        if (!$user){
-            $info($log + ['result' => false,'reason' => 'user not exists'],'result');
+        if (!$user) {
+            $info($log + ['result' => false, 'reason' => 'user not exists'], 'result');
             return false;
         }
     
         $log['user_id'] = $user->getId();
     
-        $cmd = $this->createCmd($user,$routing_key);
-    
+        $cmd = $this->createCmd($user, $routing_key);
+        
         $log['command'] = $cmd->getCommandName();
-        $log['action']  = $cmd->getActionName();
+        $log['action'] = $cmd->getActionName();
     
-        if (!$cmd->checkAccess($permission,$resource)){
-            $info($log + ['result' => false,'reason' => 'not access'],'result');
+        if (!$cmd->checkAccess($permission, $resource)) {
+            $info($log + ['result' => false, 'reason' => 'not access'], 'result');
             return false;
         }
-        $info($log + ['result' => true,'reason' => 'default'],'result');
+        $info($log + ['result' => true, 'reason' => 'default'], 'result');
         return true;
     }
 }
